047669b0ea64b64f2e0cc383ace6a47540d62b44
Alessandro Amici <alexamici@gmail.com>
2016-03-18 22:39:47 +0100

    Typo fix.

--- elevation/api.py
+++ elevation/api.py
@@ -12,4 +12,4 @@ def clip(bbox, out_path, dataset='srtm3', provider='cgiar-csi', version='V41'):
     if dataset.lower() == 'srtm3' and provider.lower() == 'cgiar-csi' and version in ['V41']:
         return cgiar_csi.srtm3_clip(xmin, ymin, xmax, ymax, out_path, version=version)
 
-    raise ValueError("Unknown provider/dataset/version combination: %r/%r/%r" % (provider/dataset, version))
+    raise ValueError("Unknown provider/dataset/version combination: %r/%r/%r" % (provider, dataset, version))
