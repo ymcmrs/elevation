e933de73f9aa560822cf2662d221039475e1cb6c
Alessandro Amici <alexamici@gmail.com>
2016-03-20 10:36:00 +0100

    Fix packagng and old tests.

---
+++ tests/test_util.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 Alessandro Amici
+#
+
+# python 2 support via python-future
+from __future__ import absolute_import, unicode_literals
+
+from elevation import util
